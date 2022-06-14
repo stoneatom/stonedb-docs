403b411cea28c12f366b57ae8e7b2593b6debcf0
Ma Jun <64419837+leizzboy@users.noreply.github.com>
2022-06-14 11:26:48 +0800

    Create sidebars.js

---
+++ docs/sidebars.js
@@ -0,0 +1,18 @@
+module.exports = {
+    docs: [
+      {
+        type: 'category',
+        label: 'About StoneDB',
+        collapsible: true,
+        collapsed: false,
+        items: [
+          'creating-pages',
+          {
+            type: 'category',
+            label: 'About StoneDB',
+            items: ['intro', 'download'],
+          },
+        ],
+      },
+    ],
+  };
